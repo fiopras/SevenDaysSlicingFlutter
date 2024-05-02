cd6630e4e2572893015bb07a336f0b037c2b0c19
Fio Prasetyo <fioprasetyo1689@gmail.com>
2024-05-02 10:56:31 +0700

    Update Slicing Day Six Part Two

--- lib/day_six_part_two.dart
+++ lib/day_six_part_two.dart
@@ -6,13 +6,74 @@ class DaySixPartTwo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
-      body: Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [Color(0xff0B073E), Color(0xff8C43FB)],
-        )),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/bg-linier.png',
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 80),
+            child: Column(
+              children: [
+                Center(
+                  child: Image.asset(
+                    'assets/pro-features.png',
+                    width: 164,
+                  ),
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+                Text(
+                  'Pro Features',
+                  style: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontSize: 22,
+                      fontWeight: FontWeight.w600),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Unlock the winner modules\nand get more insights',
+                  style: GoogleFonts.poppins(
+                    color: Color(0xff7F7FAD),
+                    fontSize: 16,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        Image.asset(
+                          'assets/ic_check_features.png',
+                          width: 26,
+                        ),
+                        SizedBox(
+                          width: 12,
+                        ),
+                        Text(
+                          'Unlock Our Top Charts',
+                          style: GoogleFonts.poppins(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
